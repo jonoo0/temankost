27e8de047f3882a9af4bdfc20c1215c5bb7c625e
Kharisma Gilang <kharisma.gilang@students.utdi.ac.id>
2024-06-08 23:17:41 +0700

    bypass insecure html

--- resources/views/laporan_penjualan_pdf.blade.php
+++ resources/views/laporan_penjualan_pdf.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Laporan Penjualan</title>
 
     {{-- <link rel="stylesheet" href="{{ asset('/AdminLTE-2/bower_components/bootstrap/dist/css/bootstrap.min.css') }}"> --}}
